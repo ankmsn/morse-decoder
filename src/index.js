1c8751b28e9bc98080800ec078d2c12e3824f7d2
ankmsn <khomstyle@gmail.com>
2023-01-17 20:27:25 +0000

    init: done

--- src/index.js
+++ src/index.js
@@ -38,7 +38,12 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    MORSE_TABLE['*'] = ' ';
+   let result ='';
+   for (i=0; i<expr.length; i += 10) {
+result += MORSE_TABLE[expr.slice(0 + i, 10 + i).replaceAll('10', '.').replaceAll('11','-').replaceAll('00','').replaceAll('**********','*')]; 
+}
+   return result;
 }
 
 module.exports = {
